d52ab9389a9d06ff7950198c0be6f458cd6969c3
Noor Alam <alamrangrej787@gmail.com>
2023-09-24 03:05:54 +0530

    first Page

---
+++ next UI/src/components/circle/BreathingCircle.jsx
@@ -0,0 +1,12 @@
+import './BreathingCircle.css';
+
+// const BreathingCircle = ({ text, animation }) => {
+const BreathingCircle = () => {
+  return (
+    <div className={`breathing-circle `}>
+      <div className="text"></div>
+    </div>
+  );
+};
+
+export default BreathingCircle;
---
+++ next UI/src/components/smoke/SmokeBackground.jsx
@@ -0,0 +1,13 @@
+// import "./SmokeBackground.css";
+
+const SmokeBackground = () => {
+  return (
+    <>
+      <div className="smoke-container">
+        <div className="smoke">Alam</div>
+      </div>
+    </>
+  );
+};
+
+export default SmokeBackground;
